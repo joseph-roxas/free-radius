ca1cf93c49ba753c55f94a3f38fca7bfe14484d1
Joseph Roxas <joseph-roxas@users.noreply.github.com>
2020-10-25 22:19:57 +0800

    Create add_user_1_Min.sql

---
+++ Sql/add_user_1_Min.sql
@@ -0,0 +1,43 @@
+# Add a new user to radcheck
+
+SET @random_chars :=
+        (SELECT CONCAT(CHAR(FLOOR(65 + RAND()*26)),
+               CHAR(FLOOR(65 + RAND()*26)),
+               CHAR(FLOOR(65 + RAND()*26)),
+               CHAR(FLOOR(65 + RAND()*26)),
+               CHAR(FLOOR(65 + RAND()*26))));
+
+SET @random_pass :=
+        (SELECT CONCAT(CHAR(FLOOR(48 + RAND()*10)),
+               CHAR(FLOOR(48 + RAND()*10)),
+               CHAR(FLOOR(48 + RAND()*10)),
+               CHAR(FLOOR(48 + RAND()*10))));
+
+INSERT INTO radcheck
+        (
+        username
+        ,attribute
+        ,op
+        ,value
+        )
+VALUES
+        (
+        @random_chars,
+        "Cleartext-Password",
+        ":=",
+        @random_pass
+        ),
+        (
+        @random_chars,
+        "Max-All-Session",
+        ":=",
+        "60"
+        ),
+        (
+        @random_chars,
+        "Simultaneous-Use",
+        ":=",
+        "1"
+        );
+
+SELECT  @random_chars, @random_pass;
